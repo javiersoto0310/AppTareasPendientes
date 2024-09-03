7c7fd8b835142d0af08861c613b772079082308d
javiersoto0310 <javier.soto@alumnos.ispi4038.edu.ar>
2024-09-03 00:23:30 -0300

    test eliminar tarea

--- src/tareasPendinetes.py
+++ src/tareasPendinetes.py
@@ -6,7 +6,15 @@ class ListaDeTareas:
         tarea = {"descripcion": descripcion, "completada": False}
         self.tareas.append(tarea)
 
+    def obtener_tarea(self, descripcion):
+        for tarea in self.tareas:
+            if tarea["descripcion"] == descripcion:
+                return tarea
+
     def marcar_como_completada(self, descripcion):
         for tarea in self.tareas:
             if tarea["descripcion"] == descripcion:
                 tarea["completada"] = True
+
+    def eliminar_tarea(self, descripcion):
+        self.tareas = [tarea for tarea in self.tareas if tarea["descripcion"] != descripcion]
--- test/testTareasPendientes.py
+++ test/testTareasPendientes.py
@@ -12,3 +12,11 @@ def test_marcar_como_completada():
     lista.agregar_tarea("Comprar leche")
     lista.marcar_como_completada("Comprar leche")
     assert lista.tareas[0]["completada"] is True
+
+def test_eliminar_tarea():
+    lista = ListaDeTareas()
+    lista.agregar_tarea("Comprar leche")
+    lista.agregar_tarea("Comprar pan")
+    lista.eliminar_tarea("Comprar leche")
+    assert len(lista.tareas) == 1
+    assert lista.tareas[0]["descripcion"] == "Comprar pan"
